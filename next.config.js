ddfd0f0a70da19a87d2033d0bebf989f5aa6024d
@l_aldi <aldikurniawan1606@gmail.com>
2023-10-10 11:29:00 +0700

    add config for next Image

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const withReactSvg = require("next-react-svg");
+const path = require("path");
 
-module.exports = nextConfig;
+module.exports = withReactSvg({
+  reactStrictMode: true,
+  compress: true,
+  include: path.resolve(__dirname, "src/assets"),
+  webpack(config, options) {
+    return config;
+  },
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+
+    return config;
+  },
+  //   images: {
+  //     domains: ["assets.nbatopshot.com", "45.77.252.22"],
+  //   },
+  //   async rewrites() {
+  //     return [
+  //       {
+  //         source: "/api/:path*",
+  //         destination: "http://45.77.252.22:9069/:path*",
+  //       },
+  //     ];
+  //   },
+});
